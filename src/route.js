b9b6db9f963d91f499497cb13079b8a518775b5d
dianadev <dianadutra.sc@gmail.com>
2021-09-17 18:34:04 +0100

    Inserting data, creating rooms and questions

--- src/route.js
+++ src/route.js
@@ -10,7 +10,11 @@ const route = express.Router();
 route.get('/index', (req, res)=> res.render("index",{page:'enter-room'}));
 route.get('/create-pass', (req, res)=> res.render("index",{page:'create-pass'}));
 
-route.get('/room/:room', (req, res)=> res.render("room"));
+route.post('/create-room', roomController.create);
+route.get('/room/:room', roomController.open);
+
+
+route.post('/question/create/:room', questionController.create);
 /*Create a route like that: room/password/question/action */
 
 //Format that the form has to pass the information/parameters to the backend route
@@ -19,7 +23,6 @@ route.get('/room/:room', (req, res)=> res.render("room"));
 //route.get('/room/:room/:question/:action', (req,res) => res.render("example",{req}))
 
 route.post('/question/:room/:question/:action', questionController.index); /*(req,res) stays implicit*/
-route.post('/create-room', roomController.create);
 
 
 module.exports = route;
